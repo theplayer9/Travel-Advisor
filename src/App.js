118a61870c6026a0b053c496ff9730daea017365
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-02 17:36:58 +0530

    made seperate folders for different items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React from "react";
+import {CssBaseline, Grid} from '@material-ui/core'
+
+
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Map from './components/Map/Map'
+import PlaceDetails from './components/PlaceDetails/PlaceDetails'
+
+
 
 const App = ()=> {
     return ( 
