e1f5e7ea33e602b42db32522ef3fe46e55853e2a
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-03 23:03:07 +0530

    Added dependencies in header.jsx file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import {Autocomplete} from '@react-google-maps/api'
+import {AppBar, Toolbar, Typography, InputBar, Box} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons'
+
+
 
 const Header = ()=>{
     return (
